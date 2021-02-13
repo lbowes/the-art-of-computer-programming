a4d199bc0a5c872315f19dcdadd0a5bfcf7aa746
Lewis Bowes <lewisbowes0@gmail.com>
2021-02-13 16:03:40 +0000

    Update .gitignore

--- vol-1/1.1E/main.c
+++ vol-1/1.1E/main.c
@@ -2,6 +2,8 @@
 Euclid's Algorithm
 "Given two positive integers `m` and `n`, find their greatest common divisor,
 i.e, the largest positive integer which evenly divides both `m` and `n`.
+
+> gcc -Wall main.c -o 1-1E.out && ./1-1E.out 15 6
 */
 #include <stdio.h>
 #include <stdlib.h>
